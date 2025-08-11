e265582607ad5bba60ae0021f6e43b2a0f25251b
hugo <vvhooping@gmail.com>
2025-08-11 11:21:57 +0900

    solve: Q1_F_BST 풀이

--- Data-Structures/Binary_Search_Tree/Q1_F_BST.c
+++ Data-Structures/Binary_Search_Tree/Q1_F_BST.c
@@ -91,32 +91,61 @@ int main()
 
 //////////////////////////////////////////////////////////////////////////////////
 
-void levelOrderTraversal(BSTNode* root)
-{
+void levelOrderTraversal(BSTNode* root) {
+	/* add your code here */
+	Queue queue;
+	queue.head = NULL;
+	queue.tail = NULL;
 
-    /* add your code here */
+	enqueue(&queue.head, &queue.tail, root);
+
+	while (!isEmpty(queue.head)) {
+		BSTNode* current_node = dequeue(&queue.head, &queue.tail);
+		printf("%d ", current_node->item);
+
+		if (current_node->left != NULL) {
+			enqueue(&queue.head, &queue.tail, current_node->left);
+		}
+
+		if (current_node->right != NULL) {
+			enqueue(&queue.head, &queue.tail, current_node->right);
+		}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
+
+/*
+ * 이진 탐색 트리에 새로운 노드를 삽입하는 함수
+ * 매개변수:
+ *   - node: 이진 탐색 트리의 노드에 대한 이중 포인터
+ *   - value: 삽입할 값
+ * 동작:
+ *   1. 노드가 NULL인 경우 새로운 노드를 생성하고 값을 저장
+ *   2. 삽입할 값이 현재 노드의 값보다 작으면 왼쪽 서브트리로 이동
+ *   3. 삽입할 값이 현재 노드의 값보다 크면 오른쪽 서브트리로 이동
+ *   4. 값이 이미 존재하는 경우 삽입하지 않고 반환
+ */
 void insertBSTNode(BSTNode **node, int value){
-	if (*node == NULL)
+	if (*node == NULL) // 현재 노드가 비어있는지 확인 -> 트리가 비어있거나, 새로운 노드를 삽입할 위치를 찾았을 때
 	{
+		// BSTNode 구조체 크기만큼 힙(heap) 메모리를 동적으로 할당받기
 		*node = malloc(sizeof(BSTNode));
 
-		if (*node != NULL) {
-			(*node)->item = value;
-			(*node)->left = NULL;
-			(*node)->right = NULL;
+		if (*node != NULL) {		// 메모리가 할당되었다면
+			(*node)->item = value;  // 새 노드의 item 에 value 를 저장
+			(*node)->left = NULL;   // 새 노드의 왼쪽 자식 포인터를 NULL 로 초기화
+			(*node)->right = NULL;  // 새 노드의 오른쪽 자식 포인터를 NULL 로 초기화
 		}
 	}
-	else
+	else  // 이미 현재 위치에 노드가 존재하는 경우
 	{
-		if (value < (*node)->item)
+		if (value < (*node)->item) // 삽입할 값이 현재 노드의 값보다 작을 경우 -> 왼쪽으로 탐색
 		{
 			insertBSTNode(&((*node)->left), value);
 		}
-		else if (value >(*node)->item)
+		else if (value >(*node)->item) // 삽입할 값이 현재의 노드의 값보다 클 경우 -> 오른쪽으로 탐색
 		{
 			insertBSTNode(&((*node)->right), value);
 		}
@@ -128,58 +157,83 @@ void insertBSTNode(BSTNode **node, int value){
 //////////////////////////////////////////////////////////////////////////////////
 
 // enqueue node
+/**
+ *
+ * @param headPtr : 큐의 맨 앞을 가리키는 head 포인터 주소
+ * @param tailPtr : 큐의 맨 뒤를 가리키는 tail 포인터 주소
+ * @param node : 큐에 담을 이진 탐색 트리 노드의 포인터
+ */
 void enqueue(QueueNode **headPtr, QueueNode **tailPtr, BSTNode *node)
 {
 	// dynamically allocate memory
 	QueueNode *newPtr = malloc(sizeof(QueueNode));
 
 	// if newPtr does not equal NULL
-	if (newPtr != NULL) {
-		newPtr->data = node;
-		newPtr->nextPtr = NULL;
+	if (newPtr != NULL) {		// 메모리 할당에 성공했으면
+		newPtr->data = node;    // 큐 노드의 data 필드에 node 주소 저장.
+		newPtr->nextPtr = NULL; // 새 노드는 큐의 마지막에 위치하니까 다음 노드를 가리키는 포인터는 NULL 로 초기화.
 
 		// if queue is empty, insert at head
-		if (isEmpty(*headPtr)) {
-			*headPtr = newPtr;
+		if (isEmpty(*headPtr)) {   // head 포인터가 NULL 이라면 = 큐가 비어있다면
+			*headPtr = newPtr;     // 새로 만든 큐 노드를 head 포인터가 가리키도록 해야 한다.
 		}
 		else { // insert at tail
-			(*tailPtr)->nextPtr = newPtr;
+			(*tailPtr)->nextPtr = newPtr; // 큐의 마지막 노드의 nextPtr 이 새 노드를 가리키게 한다.
 		}
 
-		*tailPtr = newPtr;
+		*tailPtr = newPtr; // 새로 생성한 노드가 큐의 tail 노드가 된다.
 	}
 	else {
 		printf("Node not inserted");
 	}
 }
 
+/**
+ * 큐의 첫 번째 BSTNode 포인터를 반환한다.
+ *
+ * @param headPtr 큐의 첫 번째 노드를 가리키는 포인터의 주소
+ * @param tailPtr 큐의 마지막 노드를 가리키는 포인터의 주소
+ * @return 큐의 첫 번째 노드를 가리키는 포인터
+ */
 BSTNode* dequeue(QueueNode **headPtr, QueueNode **tailPtr)
 {
-	BSTNode *node = (*headPtr)->data;
-	QueueNode *tempPtr = *headPtr;
-	*headPtr = (*headPtr)->nextPtr;
+	BSTNode *node = (*headPtr)->data;  // 큐의 맨 앞에 있는 노드(*headPtr)에 담겨있는 BSTNode 의 포인터(data)에 접근
+	QueueNode *tempPtr = *headPtr;     // 현재 큐의 맨 앞 노드를 제거하기 위해, 해당 노드의 주소를 임시 포인터에 저장
+	*headPtr = (*headPtr)->nextPtr;    // head 포인터를 다음 노드(*headPtr->nextPtr)로 이동 = 큐의 맨 앞 노드를 제거
 
-	if (*headPtr == NULL) {
-		*tailPtr = NULL;
+	if (*headPtr == NULL) {  // 만약 head 포인터가 다음 노드로 이동했는데 NULL이 됐다면 -> 큐의 노드가 하나였다면
+		*tailPtr = NULL;     // 큐가 비워진 것이므로, tail 포인터도 NULL 로 설정한다.
 	}
 
-	free(tempPtr);
+	free(tempPtr); // 메모리 누수 방지를 위해서 임시 노드의 메모리를 해제한다.
 
 	return node;
 }
 
+/**
+ * 큐가 비어있는지 확인한다.
+ *
+ * @param head 큐의 맨 앞에 위치한 노드
+ * @return head 포인터가 NULL인지 1(true) or 0(false)
+ */
 int isEmpty(QueueNode *head)
 {
 	return head == NULL;
 }
 
+/**
+ * 이진 탐색 트리의 모든 노드를 재귀적으로 순회하며 메모리를 해제한다.
+ *
+ * @param node 메모리를 해제할 시작 노드
+ */
 void removeAll(BSTNode **node)
 {
-	if (*node != NULL)
+	if (*node != NULL) // 현재 노드가 존재한다면
 	{
-		removeAll(&((*node)->left));
-		removeAll(&((*node)->right));
-		free(*node);
-		*node = NULL;
+		removeAll(&((*node)->left));  // 왼쪽 서브트리를 재귀적으로 호출
+		removeAll(&((*node)->right)); // 오른쪽 서브트리를 재귀적으로 호출
+		free(*node);                  // 현재 노드의 메모리를 해제
+		*node = NULL;                 // 해당 노드를 가리키던 포인터를 NULL 로 설정
+									  // 이미 해제한 메모리를 가리키는 댕글링 포인터(dangling pointer) 를 방지할 수 있다.
 	}
 }
